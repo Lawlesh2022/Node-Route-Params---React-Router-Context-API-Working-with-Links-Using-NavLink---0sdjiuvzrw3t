4bc3cbd8d56eb76003e34192c5a628cd7794de90
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-09-21 01:13:58 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const router = Router();
 
 
 router.get('/:name/:age', (req, res, next) => {
+  res.send("Hello"+ req.params.name + "You are"+ req.params.age + "years old")
 })
 module.exports = router;
